5b88b47f7a102029cc64676cd057861080b3f661
isaaceffah222 <2425403747@live.gctu.edu.gh>
2026-05-30 15:47:47 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,43 @@
+# File: initial.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Isaac Fosu Effah
+# Student ID: 2425403747
+# Email: 2425403747@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# on my honor, Isaac Fosu Effah, this programming assignment is my
+# own work and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+#This program prints out my initials.
 
+    print()
+    print("...IFE")
+    print()
+
+    row1 = '...IIIIIIIIIIII........FFFFFFFFFFFF........EEEEEEEEEEEE.....'
+    row2 = '........II.............FF..................EE...............'
+    row3 = '........II.............FF..................EE...............'
+    row4 = '........II.............FF..................EE...............'
+    row5 = '........II.............FF..................EE...............'
+    row6 = '........II.............FFFFFFFFFFF.........EEEEEEEEEEE......'
+    row7 = '........II.............FF..................EE...............'
+    row8 = '........II.............FF..................EE...............'
+    row9 = '........II........**...FF.............**...EE.............**'
+    row10= '...IIIIIIIIIIII...**...FF.............**...EEEEEEEEEEEE...**'
+    print(row1)
+    print(row2)
+    print(row3)     
+    print(row4)
+    print(row5)
+    print(row6)
+    print(row7)
+    print(row8)
+    print(row9)
+    print(row10)
+    print()
 
 
 main()
